e7534dce4a443eca29793e34127a17b97dea948b
brunoyukkiwada <brunoyukkiwada@gmail.com>
2026-03-09 03:36:05 -0500

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,18 +1,36 @@
-class TurboMode(private val context: Context) {
+package com.gamebooster
 
-    fun boost() {
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-        val am = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+class MainActivity : AppCompatActivity() {
 
-        val processos = am.runningAppProcesses
+    lateinit var booster: TurboMode
+    lateinit var tempMonitor: TemperatureMonitor
+    lateinit var fpsMonitor: FPSMonitor
 
-        processos?.forEach {
+    override fun onCreate(savedInstanceState: Bundle?) {
 
-            if(it.importance != ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-                android.os.Process.killProcess(it.pid)
-            }
+        booster = TurboMode(this)
+        tempMonitor = TemperatureMonitor(this)
+        fpsMonitor = FPSMonitor()
 
+        val boostBtn = findViewById<Button>(R.id.boostButton)
+
+        boostBtn.setOnClickListener {
+
+            booster.boost()
+
+            val temp = tempMonitor.getTemp()
+
+            Toast.makeText(this, "Temp: $temp °C", Toast.LENGTH_SHORT).show()
+
+            fpsMonitor.start()
         }
 
     }
